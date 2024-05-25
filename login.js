3dad32f7c809709a47768bb6c050023324205f94
jp449 <ceh3cg@virginia.edu>
2024-05-25 18:13:05 -0400

    login screen file

---
+++ login.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function Timer({ style }) {}
+
+const styles = StyleSheet.create({
+    
+});
